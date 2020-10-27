0a32a6ee6d0ea5ba0f9b315a8226b083fce712bc
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-27 11:14:56 +0530

    Added : code for finding maximum string among three

--- test/java/com/bridgelabz/TestMaximum/TestMaximumTest.java
+++ test/java/com/bridgelabz/TestMaximum/TestMaximumTest.java
@@ -8,75 +8,75 @@ public class TestMaximumTest {
 	
 	@Test
 	//TC 1.1
-	public void givenThreeInteger_WhenFirstPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeInteger_WhenFirstPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeFirtsPositionIntegerMaximum(9,7,4);
-		Assert.assertEquals(true, result);
+		int result = testMaximum.computeIntegerMaximum(9,7,4);
+		Assert.assertEquals(9, result);
 	}
 	@Test
 	//TC 1.2
-	public void givenThreeIntegers_WhenSecondPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeIntegers_WhenSecondPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeSecondPositionIntegerMaximum(9,12,4);
-		Assert.assertEquals(true, result);
+		int result = testMaximum.computeIntegerMaximum(9,12,4);
+		Assert.assertEquals(12, result);
 	}
 	@Test
 	//TC 1.3
-	public void givenThreeIntegers_WhenThirdPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeIntegers_WhenThirdPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeThirdPositionIntegerMaximum(9,7,18);
-		Assert.assertEquals(true, result);
+		int result = testMaximum.computeIntegerMaximum(9,7,18);
+		Assert.assertEquals(18, result);
 	}
 	@Test
 	//TC 2.1
-	public void givenThreeFloat_WhenFirstPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeFloat_WhenFirstPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeFirstPositionFloatMaximum(27f, 21f, 5f);
-		Assert.assertEquals(true, result);
+		float result = testMaximum.computeFloatMaximum(27f, 21f, 5f);
+		Assert.assertEquals(27f , result);
 	}
 	@Test
 	//TC 2.2
-	public void givenThreeFloat_WhenSecondPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeFloat_WhenSecondPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeSecondPositionFloatMaximum(8f, 21f, 3f);
-		Assert.assertEquals(true, result);
+		float result = testMaximum.computeFloatMaximum(8f, 21f, 3f);
+		Assert.assertEquals(21f , result);
 	}	
 	@Test
 	//TC 2.3
-	public void givenThreeFloat_WhenThirdPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeFloat_WhenThirdPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeThirdPositionFloatMaximum(52f, 22f, 107f);
-		Assert.assertEquals(true, result);
+		float result = testMaximum.computeFloatMaximum(52f, 22f, 107f);
+		Assert.assertEquals(107f , result);
 	}
 	@Test
 	//TC 3.1
-	public void givenThreeString_WhenFirtsPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeString_WhenFirtsPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeFirstPositionStringMaximum("Life is beautiful", "empathy", "insolency");
-		Assert.assertEquals(true, result);
+		String result = testMaximum.computeStringMaximum("zeal", "empathy", "insolency");
+		Assert.assertEquals("zeal", result);
 	}
 	@Test
 	//TC 3.2
-	public void givenThreeString_WhenSecondPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeString_WhenSecondPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeSecondPositionStringMaximum("sympathy", "Everything happens is a part of life", "species");
-		Assert.assertEquals(true, result);
+		String result = testMaximum.computeStringMaximum("sympathy", "zeal", "species");
+		Assert.assertEquals("zeal", result);
 	}
 	@Test
 	//TC 3.3
-	public void givenThreeString_WhenThirdPositionValueMaximum_ShouldReturnTrue()
+	public void givenThreeString_WhenThirdPositionValueMaximum_ShouldReturnMaximum()
 	{
 		TestMaximum testMaximum =  new TestMaximum();
-		boolean result = testMaximum.computeThirdPositionStringMaximum("water", "origin", "floccinaucinihilipilification");
-		Assert.assertEquals(true, result);
+		String result = testMaximum.computeStringMaximum("water", "origin", "zeal");
+		Assert.assertEquals("zeal", result);
 	}
 	
 }
